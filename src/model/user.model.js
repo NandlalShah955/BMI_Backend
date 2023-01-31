570c046393a3fe5e8f73c130177eb90b604292e8
Nandlal <shahnandlal49@gmail.com>
2023-01-31 16:02:00 +0530

    added

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
   },
-  { versionKey: false }
+  { versionKey: false}
 );
 const Users = mongoose.model("user", userSchema);
 module.exports = Users;
